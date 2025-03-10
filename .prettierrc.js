b849e3126c147e86b2d6f8190fceab69ec0ac0a9
Zshijie <2023309181@qq.com>
2025-03-10 11:05:12 +0800

    Test Husky

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 120, // 每行代码长度（默认80）
+  tabWidth: 2, // 每个tab相当于多少个空格（默认2）
+  tabs: false, // 是否使用tab缩进（默认false）
+  semi: false, // 声明结尾使用分号(默认true)
+  singleQuote: true, // 使用单引号（默认false）
+  quoteProps: 'as-needed', // 对象的key仅在必要时用引号
+  trailingComma: 'none', // 多行使用拖尾逗号（默认none）
+  bracketSpacing: true, // 对象大括号直接是否有空格（默认true）
+  jsxBracketSameLine: false, // jsx > 是否另起一行
+  arrowParens: 'avoid', // (x) => {} 是否要有小括号
+  endOfLine: 'auto' // 结尾是 \n \r \n\r auto
+}
---
+++ pages/my/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <view>
+    <up-navbar title="个人中心" :autoBack="true"> </up-navbar>
+  </view>
+</template>
+
+<script setup>
+import uviewPlus from 'uview-plus'
+let a = '1'
+console.log(uviewPlus)
+</script>
+
+<style lang="scss" scoped></style>
