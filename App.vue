4eff96f24476f3a9486b3bb6af0eef2bb1b021b9
Zshijie <2023309181@qq.com>
2025-03-14 16:56:12 +0800

    哈哈哈

--- App.vue
+++ App.vue
@@ -7,7 +7,7 @@ export default {
     console.log('App Show', '222111')
   },
   onHide: function () {
-    console.log('App Hide', '111中文2')
+    console.log('App Hide', '111中文21')
   }
 }
 </script>
